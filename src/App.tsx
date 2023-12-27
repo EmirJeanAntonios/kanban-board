d4cace2ecef963fb543265d9db4144cef4b45fec
Emir Jean Antonios <emir.antonios@gmail.com>
2023-12-27 10:29:32 +0300

    WIP: kanban wrapper component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import "./App.scss";
+import KanbanWrapper from "./components/KanbanWrapper/KanbanWrapper";
 function App() {
-  return <main>Hello World</main>;
+  return (
+    <main>
+      <section>
+        <KanbanWrapper title="Backlog">
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </KanbanWrapper>
+      </section>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/KanbanWrapper/KanbanWrapper.tsx
@@ -0,0 +1,22 @@
+import React, { PropsWithChildren } from "react";
+import "./styles.scss"
+
+type KanbanWrapperProps = {
+  title: string;
+};
+
+const KanbanWrapper: React.FC<PropsWithChildren<KanbanWrapperProps>> = ({
+  title,
+  children,
+}) => {
+  return (
+    <div className="kanban_wrapper_container">
+      <div className="kanban_wrapper_title">
+        <h2>{title}</h2>
+      </div>
+      <div className="kanban_wrapper_content">{children}</div>
+    </div>
+  );
+};
+
+export default KanbanWrapper;
